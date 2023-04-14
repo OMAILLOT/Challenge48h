a9935dc9c9279a80e3051bf53703d7201a518c2c
Wawax007 <alexis.borteyru@gmail.com>
2023-04-14 10:05:59 +0200

    Cartes bonus argent modif script

--- Assets/QuestionManager.cs
+++ Assets/QuestionManager.cs
@@ -8,20 +8,28 @@ public class QuestionManager : MonoBehaviour
 {
     public TextMeshPro questionText;
     public List<Question> questionsList;
-    public int qipointBonus = 10; // Nombre de points QI à gagner pour une question QI bonus
+    public int qipointBonus = 10; // Nombre de points QI à gagner pour une question QI correctement répondu
+    public int qipointMalus = 10; // Nombre de points QI à perdre pour une question QI mal répondu
     public int argentBonus = 50; // Montant d'argent à gagner pour une question argent bonus
     public int argentMalus = -25; // Montant d'argent à perdre pour une question argent malus
     public int score = 0; // Score du joueur (QI ou argent, selon le type de question)
+    private BoardManager boardManager;
+
+
 
     void Start()
     {
+
+        boardManager = BoardManager.Instance;
+
         // Ajouter ici toutes les questions à afficher dans la liste "questionsList"
         questionsList = new List<Question>(); // Initialise la liste avant de la remplir
         questionsList.Add(new Question("Quel est le nom de la capitale de la France ?", "Paris", "qi"));
         questionsList.Add(new Question("Combien de planètes y a-t-il dans le système solaire ?", "8", "qi"));
-        questionsList.Add(new Question("Quel est l'inventeur de la théorie de la relativité ?", "Albert Einstein", "qi"));
-        questionsList.Add(new Question("Quel est le nom du président français actuel ?", "Emmanuel Macron", "argentBonus"));
-        questionsList.Add(new Question("Quelle est la plus haute montagne du monde ?", "Everest", "argentMalus"));
+        questionsList.Add(
+            new Question("Quel est l'inventeur de la théorie de la relativité ?", "Albert Einstein", "qi"));
+        questionsList.Add(new Question("Quel est le nom du président français actuel ?", "Emmanuel Macron", "argent"));
+        questionsList.Add(new Question("Quelle est la plus haute montagne du monde ?", "Everest", "argent"));
         questionsList.Add(new Question("En quelle année est née Marie Curie ?", "1867", "qi"));
 
         // Récupérer ici la référence de la case sur laquelle le joueur est tombé
@@ -37,6 +45,7 @@ public class QuestionManager : MonoBehaviour
             // Attendre la réponse du joueur
             StartCoroutine(WaitForAnswer(randomQuestion));
         }
+<<<<<<< Updated upstream
         else if (caseJoueur.GetComponent<Case>().type == Case.TypeCase.Malus)
         {
             // Choisir une autre question dans la liste "questionsList"
@@ -46,6 +55,8 @@ public class QuestionManager : MonoBehaviour
             // Attendre la réponse du joueur
             StartCoroutine(WaitForAnswer(randomQuestion));
         }
+=======
+>>>>>>> Stashed changes
     }
 
     IEnumerator WaitForAnswer(Question question)
@@ -55,24 +66,19 @@ public class QuestionManager : MonoBehaviour
         {
             yield return new WaitForSeconds(0.1f);
 
-            // Vérifier si le joueur a appuyé sur le bouton de validation de réponse
-            if (Input.GetKeyDown(KeyCode.Return))
+            // Vérifier si le joueur a sélectionné une réponse
+            if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                // Vérifier si la réponse est correcte
-                if (question.CheckAnswer())
+                if (question.CheckAnswer(question.options[0]))
                 {
                     // Attribuer des points ou de l'argent en fonction du type de question
-                    if (question.type == "qi")
+                    if (question.type == "qiBonus")
                     {
                         score += qipointBonus;
                     }
                     else if (question.type == "argentBonus")
                     {
                         score += argentBonus;
-                    }
-                    else if (question.type == "argentMalus")
-                    {
-                        score += argentMalus;
                     }
 
                     Debug.Log("Bonne réponse");
@@ -84,7 +90,89 @@ public class QuestionManager : MonoBehaviour
                 }
                 else
                 {
+                    if (question.type == "qi")
+                    {
+                        score += qipointMalus;
+                    }
+
+                    if (question.type == "argent")
+                    {
+                        score -= argentMalus;
+                    }
+
                     // Si la réponse est incorrecte, ne rien faire et attendre que le joueur essaye à nouveau
+                    Debug.Log("Mauvaise réponse");
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                if (question.CheckAnswer(question.options[1]))
+                {
+                    // Attribuer des points ou de l'argent en fonction du type de question
+                    if (question.type == "qi")
+                    {
+                        score += qipointBonus;
+                    }
+                    else if (question.type == "argent")
+                    {
+                        score += argentBonus;
+                    }
+
+                    Debug.Log("Bonne réponse");
+                    // Mettre à jour le score du joueur
+                    GameObject.FindGameObjectWithTag("Score").GetComponent<Text>().text = "Score : " + score;
+
+                    // Quitter la boucle de vérification de réponse
+                    break;
+                }
+                else
+                {
+                    if (question.type == "qi")
+                    {
+                        score -= qipointMalus;
+                    }
+
+                    if (question.type == "argent")
+                    {
+                        score -= argentMalus;
+                    }
+
+                    // Si la réponse est incorrecte, ne rien faire et attendre que le joueur essaye à nouveau
+                    Debug.Log("Mauvaise réponse");
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                if (question.CheckAnswer(question.options[1]))
+                {
+                    // Attribuer des points ou de l'argent en fonction du type de question
+                    if (question.type == "qi")
+                    {
+                        score += qipointBonus;
+                    }
+                    else if (question.type == "argent")
+                    {
+                        score += argentBonus;
+                    }
+
+                    Debug.Log("Bonne réponse");
+                    // Mettre à jour le score du joueur
+                    GameObject.FindGameObjectWithTag("Score").GetComponent<Text>().text = "Score : " + score;
+
+                    break;
+                }
+                else
+                {
+                    if (question.type == "qi")
+                    {
+                        score += qipointMalus;
+                    }
+
+                    if (question.type == "argent")
+                    {
+                        score -= argentMalus;
+                    }
+
                     Debug.Log("Mauvaise réponse");
                 }
             }
@@ -108,7 +196,7 @@ public class Question
     public bool CheckAnswer(string answer=null)
     {
         // Instancier l'objet InputField
-        GameObject inputFieldObject = GameObject.Find("InputField");
+        GameObject inputFieldObject = GameObject.Find("PlayerController");
 
         // Vérifier si la réponse est correcte
         if (inputFieldObject != null)
---
+++ Assets/Scripts/Class/PlayerController.cs
@@ -0,0 +1,108 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject playerRenderer;
+
+    public string playerName;
+    public int currentCoin;
+    public float knowledgePoint;
+    public bool isMyTurn = false;
+    public int playerOnCaseIndex = 0;
+    public int numberOfTurn = 0;
+    public Case nextcase;
+    public List<Case> allCaseToNextCase = new List<Case>();
+
+    int diceResult;
+    int nextCase;
+    Sequence playerMooveSequence;
+    public void StartMyTurn()
+    {
+        isMyTurn = true;
+        diceResult = Random.Range(PlayerManager.Instance.minDiceNumber, PlayerManager.Instance.maxDiceNumber);
+        UiManager.Instance.randomNumber = diceResult;
+        playerMooveSequence = DOTween.Sequence();
+    }
+
+    public void PlayerMoove()
+    {
+
+        StartCoroutine(AnimationMove());
+            //.OnComplete(PlayerMooveInCase);
+            /*.Join(playerRenderer.transform.DOLocalMoveY(2, .5f))
+            .Append(playerRenderer.transform.DOLocalMoveY(0, .5f)).OnStepComplete(() => nextCase++).SetLoops(diceResult).OnComplete(PlayerMooveInCase);*/
+    }
+
+    IEnumerator AnimationMove()
+    {
+        nextCase = playerOnCaseIndex + 1;
+        int finalIndex = playerOnCaseIndex + diceResult + 1;
+        if (nextCase == BoardManager.Instance.allCases.Count)
+        {
+            nextCase = 0;
+            finalIndex -= BoardManager.Instance.allCases.Count;
+        }
+        while (nextCase < finalIndex)
+        {
+            yield return DOTween.Sequence().Append(transform.DOMove(BoardManager.Instance.allCases[nextCase].transform.position + Vector3.up * 1, .25f))
+                .Join(playerRenderer.transform.DOLocalMoveY(2, .125f))
+                .Append(playerRenderer.transform.DOLocalMoveY(0, .125f))
+                .WaitForCompletion();
+            nextCase++;
+            if (nextCase >= BoardManager.Instance.allCases.Count)
+            {
+                nextCase = 0;
+                finalIndex -= BoardManager.Instance.allCases.Count;
+            }
+        }
+        PlayerMooveInCase();
+    }
+
+    private void PlayerMooveInCase()
+    {
+        playerOnCaseIndex += diceResult;
+        if (playerOnCaseIndex >= BoardManager.Instance.allCases.Count)
+        {
+            numberOfTurn++;
+            playerOnCaseIndex = playerOnCaseIndex - BoardManager.Instance.allCases.Count;
+        }
+
+        if (nextcase != null)
+        {
+            nextcase.ResetPlayerOnCase(this);
+        }
+        nextcase = BoardManager.Instance.allCases[playerOnCaseIndex];
+        //transform.position = nextcase.transform.position + Vector3.up * 1;
+        nextcase.OnStartCase(this);
+    }
+
+    public void OnPlayerEnter(Case currentCase)
+    {
+        if (currentCase.type == TypeCase.Chance)
+        {
+            // Si la case est de type "Bonus", ajouter de l'argent au joueur
+            currentCoin += 100;
+            Debug.Log("Vous avez gagn� 100 � !");
+        }
+/*        else if (currentCase.type == TypeCase.Malus)
+        {
+            // Si la case est de type "Malus", retirer de l'argent au joueur
+            currentCoin -= 50;
+            Debug.Log("Vous avez perdu 50 � !");
+        }*/
+        else if (currentCase.type == TypeCase.qiPoints)
+        {
+            // Si la case est de type "QI", retirer du QI au joueur
+            knowledgePoint -= 50;
+            Debug.Log("Vous avez perdu 50 points de QI !");
+        }
+        else
+        {
+            // Si la case est de type "Normal", ne rien faire
+            Debug.Log("Case normale !");
+        }
+    }
+}
